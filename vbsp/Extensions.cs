6f305df11d1f7735dbadfa86b65f7d4e83ff7199
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2014-06-11 23:22:09 -0400

    Added Project

---
+++ vbsp/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace vbsp
+{
+    public static class Extensions
+    {
+        /// <summary>Indexes the of NTH.</summary>
+        /// <param name="str">The string.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="nth">The NTH.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentException">Can not find the zeroth index of substring in string. Must start with 1</exception>
+        public static int IndexOfNth(this string str, string value, int nth = 1)
+        {
+            if (nth < 0)
+                throw new ArgumentException("Can not find the zeroth index of substring in string. Must start with 1");
+            int offset = str.IndexOf(value);
+            for (int i = 1; i < nth; i++)
+                if (offset == -1) return -1;
+                else offset = str.IndexOf(value, offset + 1);
+            return offset;
+        }
+    }
+}
---
+++ vbsp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace vbsp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Call alternate compilers
+            var ccTagIntermediateCompilerProcess = new Process();
+            ccTagIntermediateCompilerProcess.StartInfo.FileName = "CCTagIntermediateCompiler.exe";
+            ccTagIntermediateCompilerProcess.StartInfo.Arguments = Environment.GetCommandLineArgs().Last() + ".vmf";
+            ccTagIntermediateCompilerProcess.Start();
+            ccTagIntermediateCompilerProcess.WaitForExit();
+
+
+            //Call standard compiler
+            var portal2Process = new Process();
+            portal2Process.StartInfo.FileName = "vbsp_original.exe";
+            portal2Process.StartInfo.Arguments = Environment.CommandLine.Substring(Environment.CommandLine.IndexOf("exe\"") + 4).Trim();
+            portal2Process.Start();
+            portal2Process.WaitForExit();
+        }
+    }
+}
